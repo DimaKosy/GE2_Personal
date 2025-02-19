a6b5adb5f8b7e16343b0b6b69f769671aa587e5c
Dmytro Kosynskyy <dmytro.kosynskyy@stud.h-da.de>
2025-02-19 03:23:47 +0000

    camera movements

--- week1/boid.gd
+++ week1/boid.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 @export var target:Node3D
-#@export var force:Node3D
+@export var force:Vector3
 #@export var acc:Vector3
 
 @export var mass:float = 1
@@ -15,7 +15,7 @@ func _process(delta: float):
 	
 	var accel = force/mass
 	
-	velocity = Vector3(0,0,1)
+	velocity = velocity + accel
 	
 	move_and_slide()
 	
@@ -25,4 +25,14 @@ func seek(target:Node3D) -> Vector3:
 	var to_target = target.global_position - global_position
 	var desired = to_target.normalized() * max_speed
 	
+	#print(to_target.x, " : ", to_target.z)
+	draw_gizmos()
+	
 	return  desired - velocity
+	
+func draw_gizmos():
+	DebugDraw3D.draw_arrow(global_position, global_position + force, Color.AQUAMARINE, 0.1)
+	DebugDraw3D.draw_arrow(global_position, global_position + global_basis.x * 10, Color.AQUAMARINE, 0.1)
+	DebugDraw3D.draw_arrow(global_position, global_position + velocity, Color.CRIMSON, 0.1)
+	DebugDraw3D.draw_arrow(global_position, global_position + global_basis.y * 10, Color.CRIMSON, 0.1)
+	DebugDraw2D.set_text("Velocity: ", velocity)
