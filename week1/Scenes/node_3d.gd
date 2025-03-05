69e964a2c6d4c7eaff16f13f1683eab5e63ce153
Dmytro Kosynskyy <dmytro.kosynskyy@stud.h-da.de>
2025-03-05 03:32:13 +0000

    began body part setup

--- week1/Scenes/node_3d.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D
-
-func _ready() -> void:
-	var lib = load("res://bin/mylib.dll")
-	
-	print(lib.add(1,1))
--- week1/gdns/mylib.gd
+++
@@ -1,8 +0,0 @@
-extends NativeScript
-
-# Declare functions from DLL
-func add_numbers(a: int, b: int) -> int:
-    return add_numbers(a, b)
-
-func print_hello():
-    print_hello()
