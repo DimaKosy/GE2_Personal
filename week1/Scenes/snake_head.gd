5dfaa38247a7fbdcb2fb3e515ccc2fd1d84b253d
Dmytro Kosynskyy <dmytro.kosynskyy@stud.h-da.de>
2025-03-11 13:15:19 +0000

    raycast avoidance

--- week1/Scenes/snake_head.gd
+++ week1/Scenes/snake_head.gd
@@ -1,29 +1,69 @@
 extends CharacterBody2D
 
 @export var mass = 1
-@export var max_speed = 50
+@export var max_speed = 80
 @export var circle_distance = 50
 @export var circle_size = 50
 var force = Vector2(0,0)
 var ran_dir = 0.0
-var circle_center = position
-var circle_point = position
+var time = 0
+var ray_length = 400
 
+var ray_origin = Vector2(0,0)
+var ray_target1 = Vector2(0,0)
+var ray_target2 = Vector2(0,0)
 @export var speed:float = 100	
 
+func ray_detection():
+	var space_state = get_world_2d().direct_space_state
 
+	ray_origin = global_position
+	ray_target1 = global_position + Vector2(ray_length, 0).rotated(deg_to_rad(20)).rotated(rotation)
+	ray_target2 = global_position + Vector2(ray_length, 0).rotated(deg_to_rad(-20)).rotated(rotation)
+		
+	var query1 = PhysicsRayQueryParameters2D.create(ray_origin, ray_target1)
+	var query2 = PhysicsRayQueryParameters2D.create(ray_origin, ray_target2)
+	query1.collide_with_areas = false  # Usually you only want to hit physics bodies
+	query2.collide_with_bodies = true
+
+	var result1 = space_state.intersect_ray(query1)
+	var result2 = space_state.intersect_ray(query1)
+
+	if result1:
+		var collider = result1.get("collider")
+		print(collider)
+		if collider is TileMapLayer:
+			print("Ray hit a TileMap with collision at position: ", result1.position)
+			ran_dir += deg_to_rad(30 * ray_length/(result1.position - global_position).length())
+	elif result2:
+		var collider = result2.get("collider")
+		print(collider)
+		if collider is TileMapLayer:
+			print("Ray hit a TileMap with collision at position: ", result2.position)
+			ran_dir -= deg_to_rad(30 * ray_length/(result2.position - global_position).length())
+	pass
 
 func wander(delta: float) -> Vector2:
 	var direction = Vector2(1,0)
 	
-	direction = direction.rotated(PI/2)
+	time += delta
+	if time > 0.5:
+		time = 0
+		ran_dir += randf_range(-PI/3,PI/3)
+		ran_dir = clamp(ran_dir, rotation - deg_to_rad(90), rotation + deg_to_rad(90))
+		
+	rotation = lerp(rotation, ran_dir, 0.02)
+	
+	direction = direction.rotated(rotation)
 	
 	return direction
 
 
-func _process(delta: float):	
+func _process(delta: float):
+	
 	force = Vector2(0,0)
 	force += wander(delta)
+	ray_detection()
 	
 	velocity += force * max_speed * delta
 	
@@ -38,8 +78,7 @@ func _process(delta: float):
 	queue_redraw()
 
 func _draw() -> void:
-	
-	draw_circle(circle_center - global_position, circle_size,Color(255,0,0),false,1)
-	draw_line(circle_center - global_position, circle_point - global_position,Color(255,0,0),1)
-	draw_line(Vector2(0,0), 300*force.rotate(-velocity.angle()), Color(255,0,255), 1)
+	draw_line(Vector2(0,0), 300*force.rotated(-velocity.angle()), Color(255,0,255), 1)
+	draw_line(ray_origin - global_position, (ray_target1 - global_position).rotated(-rotation), Color.RED,3)
+	draw_line(ray_origin - global_position, (ray_target2 - global_position).rotated(-rotation), Color.RED,3)
 	pass
