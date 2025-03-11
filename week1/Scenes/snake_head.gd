dba665814c64f71b3f34163414ffe05b67a940bd
Dmytro Kosynskyy <dmytro.kosynskyy@stud.h-da.de>
2025-03-11 05:48:14 +0000

    mild steering based on area 2D

--- week1/Scenes/snake_head.gd
+++ week1/Scenes/snake_head.gd
@@ -4,12 +4,40 @@ extends CharacterBody2D
 @export var max_speed = 50
 @export var circle_distance = 100
 var force = Vector2(0,0)
+var col_force = Vector2(0,0)
 var ran_dir = 0
 var circle_center = position
 var circle_point = position
 
-
 @export var speed:float = 100
+
+func _ready() -> void:
+	find_child("left").body_entered.connect(left_enter)
+	find_child("center").body_exited.connect(center_enter)
+	find_child("right").body_exited.connect(right_enter)
+	find_child("left").body_entered.connect(left_exit)
+	find_child("center").body_exited.connect(center_exit)
+	find_child("right").body_exited.connect(right_exit)
+
+func left_enter(body: Node) -> void:
+	col_force += Vector2(sin(rotation), cos(rotation))
+
+func center_enter(body: Node) -> void:
+	col_force += Vector2(-cos(rotation), sin(rotation))
+	pass
+	
+func right_enter(body: Node) -> void:
+	col_force += Vector2( sin(rotation), -cos(rotation))
+
+func left_exit(body: Node) -> void:
+	col_force = Vector2(sin(rotation), cos(rotation))
+
+func center_exit(body: Node) -> void:
+	col_force -= Vector2(-cos(rotation), sin(rotation))
+	pass
+	
+func right_exit(body: Node) -> void:
+	col_force -= Vector2( sin(rotation), -cos(rotation))
 
 func wander(delta: float) -> Vector2:
 	ran_dir = ran_dir + (0.1 * PI * (randf() - 0.5))
@@ -26,7 +54,12 @@ func wander(delta: float) -> Vector2:
 
 
 func _process(delta: float):	
-	velocity += wander(delta)
+	force += wander(delta)
+	col_force = col_force.normalized()
+	force += col_force
+	
+	velocity += force
+	force = (Vector2(0,0))
 	move_and_slide()
 	if velocity.length() > 0:
 		var target_rotation = velocity.angle()  
